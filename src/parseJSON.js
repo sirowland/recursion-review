ab57ea506216eb3b9b398787d22ecc719dc38428
sirowland <srowland2594@gmail.com>
2018-04-23 16:26:40 -0700

    Attempted parseJSON.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,23 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  if (json[0] === '[') {
+    //arrays
+    var tempString = json.slice(1, json.length - 1);
+    var arrayOfValues = tempString.split(',');
+    var result = arrayOfValues.map(function(element) {
+      return parseJSON(element);
+    });
+
+    return result;
+  } else if (json[0] === '{') {
+    //objs
+    
+  } else if (json[0] === '"') {
+    //strings   
+    return json.slice(1, json.length - 1);
+  } else {
+    //keywords
+    return json;
+  }
 };
